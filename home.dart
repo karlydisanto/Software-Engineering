0020e37ca0e3672a721a84fc25b2b6df02264794
MichaelFodi <70722412+MichaelFodi@users.noreply.github.com>
2020-11-19 12:54:45 -0500

    Add files via upload

--- home.dart
+++ home.dart
@@ -2,21 +2,29 @@ import 'package:fit4u/services/auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/svg.dart';
+import 'package:fit4u/screens/details_screen.dart';
+import 'package:fit4u/widgets/bottom_nav_bar.dart';
+import 'package:fit4u/widgets/category_card.dart';
+import 'package:fit4u/widgets/search_bar.dart';
+
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery
+        .of(context)
+        .size; //this gonna give us total height and with of our device
     return Scaffold(
-      backgroundColor: Colors.teal[50],
       appBar: AppBar (
         title: Text("Fit4U",
-        style: TextStyle(
-          color: Colors.black
+          style: TextStyle(
+              color: Colors.black
+          ),
         ),
-        ),
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xFFF2BEA1),
         elevation: 0.0,
         actions: [
           FlatButton.icon(
@@ -28,6 +36,91 @@ class Home extends StatelessWidget {
           )
         ],
       ),
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            // Here the height of the container is 45% of our total height
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Good Morning Michael",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .display1
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  SearchBar(),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          title: "Cardio",
+                          svgSrc: "assets/icons/cardio3.svg",
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: "Stretching",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: "Arm Exercises",
+                          svgSrc: "assets/icons/arms.svg",
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return DetailsScreen();
+                              }),
+                            );
+                          },
+                        ),
+                        CategoryCard(
+                          title: "Core Exercises",
+                          svgSrc: "assets/icons/yoga.svg",
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
+   }
   }
-}
