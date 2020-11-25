fd6be2b0b6eb4f1369ac03f1afdb7c0502e45f3f
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-25 16:14:21 -0500

    Create survey.dart

---
+++ survey.dart
@@ -0,0 +1,12 @@
+class Survey{
+  final String name;
+  final String gender;
+  final int age;
+  final String fitnessType;
+  final String fitnessLevel;
+  final int workoutDays;
+  final String goal;
+  final String exercises;
+  
+  Survey({this.name,this.gender, this.age, this.fitnessType, this.fitnessLevel, this.workoutDays, this.goal, this.exercises});
+}
