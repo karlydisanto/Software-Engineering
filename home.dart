7a527ed4a61f87326e153fe1249ace9e14be3eb9
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-25 16:16:46 -0500

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,92 +1,203 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fit4u/services/database.dart';
+import 'package:fit4u/services/auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:fit4u/shared/constants.dart';
-import 'file:///C:/Users/kdisa/AndroidStudioProjects/fit4u/lib/widgets/search_bar.dart';
-import 'package:fit4u/screens/home.dart';
-import 'package:provider/provider.dart';
+import 'package:fit4u/services/exercise_database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fit4u/screens/exerciseList.dart';
+import 'package:fit4u/screens/profile.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fit4u/screens/workout_screen.dart';
+import 'package:fit4u/widgets/category_card.dart';
+import 'package:fit4u/widgets/search_bar.dart';
+import 'package:fit4u/screens/help.dart';
+class Home extends StatelessWidget {
 
-class Survey extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return StreamProvider<QuerySnapshot>.value(
-      value: DatabaseService().fit4u,
-      child: Scaffold(
-          appBar: AppBar(
-            elevation: 0.0,
-            backgroundColor: Colors.teal[100],
-            actions: [
-              /*FlatButton.icon(
-              icon: Icon(Icons.arrow_back),
-                label: Text(""),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Home())
-                  );
-                }
-                )*/
-            ],
+    var size = MediaQuery
+        .of(context)
+        .size; //this gonna give us total height and with of our device
+    return Scaffold(
+        appBar: AppBar (
+          title: Text("Fit4U",
+            style: TextStyle(
+                color: Colors.black
+            ),
           ),
-          body: StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("exercises").snapshots(),
-              builder: (context, snapshot) {
-                return Stack(
+          backgroundColor: Colors.teal[50],
+          elevation: 0.0,
+          actions: [
+            FlatButton.icon(
+                onPressed: () async{
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text("Log out")
+            )
+          ],
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              // Here the height of the container is 45% of our total height
+              height: size.height * .45,
+              decoration: BoxDecoration(
+                color: Colors.teal[50],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage(""),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Container(
-                      height: size.height * .45,
-                      decoration: BoxDecoration(
-                        color: Colors.teal[100],
-                        //image: DecorationImage(
-                        // image: AssetImage("assets/images/meditation_bg.png"),
-                        // fit: BoxFit.fitWidth,
-                        // ),
-                      ),
+                    Text(
+                      "Welcome to Fit4U!",
+                      textAlign: TextAlign.center,
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .display1
+                          .copyWith(fontWeight: FontWeight.w900),
+
                     ),
-                    SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              SizedBox(
-                                height: size.height * 0.05,
-                              ),
-                              Text(
-                                "Registration Survey",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .display1
-                                    .copyWith(fontWeight: FontWeight.w900),
-                              ),
-                              SizedBox(height: 10),
-                              Text(
-                                "So we can learn a tailor workouts specifically to you!",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 10),
-                              SizedBox(
-                                width: size.width * .6, // it just take 60% of total width
-                                child: Text(
-                                  "It's time to pump some iron!",
+                    Row(
+                      children: [
+                        SizedBox(height: 100,),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 190,
+                          width: 170,
+                          child: Container(
+                            color: Colors.grey[400],
+                            child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0.0),
+                                side: BorderSide(color: Colors.black)
                                 ),
+                              padding: EdgeInsets.only(top: 50,),
+                              child: Column(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.fitness_center),
+                                    iconSize: 50,
+                                  ),
+                                  SizedBox(height: 15,),
+                                  Text(
+                                    "Begin Workout",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
+                              onPressed: () {
+                                Navigator.push(
+                                    context, MaterialPageRoute(builder: (context) => WorkoutScreen())
+                                );
+                              }
+                            ),
                           ),
                         ),
+                        SizedBox(width: 30,),
+                        SizedBox(
+                          height: 190,
+                          width: 170,
+                          child: Container(
+                            color: Colors.grey[400],
+                            child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0.0),
+                                  side: BorderSide(color: Colors.black)
+                                ),
+                                padding: EdgeInsets.only(top: 50,),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(Icons.settings),
+                                      iconSize: 50,
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Text(
+                                      "Help",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context, MaterialPageRoute(builder: (context) => HelpPage())
+                                  );
+                                }
+                            ),
+                          ),
+                        ),
+                        ], //children
                       ),
+                    Row(
+                      children: [
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 190,
+                          width: 170,
+                          child: Container(
+                            color: Colors.grey[400],
+                            child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0.0),
+                                    side: BorderSide(color: Colors.black)
+                                ),
+                                padding: EdgeInsets.only(top: 50,),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(Icons.person),
+                                      iconSize: 50,
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Text(
+                                      "Profile",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context, MaterialPageRoute(builder: (context) => Profile())
+                                  );
+                                }
+                            ),
+                          ),
+                        ),
+                      ], //children
                     ),
                   ],
-                );
-              }
-          )
-      ),
-    );
+              ),
+            ),
+           ),
+          ],
+        ),
+      );
   }
 }
