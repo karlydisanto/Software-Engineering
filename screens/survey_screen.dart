05dd4dbe602beba162a9d4728442650ff6b85cfa
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-25 16:25:44 -0500

    Create survey_screen.dart

---
+++ screens/survey_screen.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fit4u/services/exercise_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fit4u/shared/constants.dart';
+import 'file:///C:/Users/kdisa/AndroidStudioProjects/fit4u/lib/widgets/search_bar.dart';
+import 'package:fit4u/screens/home.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fit4u/screens/exerciseList.dart';
+
+class Survey extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    child:
+    Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.teal[100],
+        actions: [
+        ],
+      ),
+    );
+  }
+}
