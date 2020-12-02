039228306057623c6c2645129b9f654f12e02621
MichaelFodi <70722412+MichaelFodi@users.noreply.github.com>
2020-12-02 17:16:51 -0500

    Add files via upload

--- screens/sign_in.dart
+++ screens/sign_in.dart
@@ -24,92 +24,153 @@ class _SignInState extends State<SignIn> {
   String password = "";
   String error = "";
 
+
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: Text("Sign in to Fit4U",
-        style: TextStyle(
-          color: Colors.teal
-        ),
-        ),
-        actions: [FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text("Register")
-        )
-        ],
-        ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.teal,
-                    shape: BoxShape.circle
+    if (loading) {
+      return Loading();
+    } else {
+      return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 110.0, 0.0, 0.0),
+                    child: Text('Welcome',
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
                   ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10.0, 175.0, 0.0, 0.0),
+                    child: Text('Fit4U',
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(190.0, 175.0, 0.0, 0.0),
+                    child: Text('.',
+                        style: TextStyle(
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.tealAccent)),
+                  )
+                ],
               ),
-              SizedBox(height: 50),
-              TextFormField(
-                validator: (val) => val.isEmpty ? "Enter an email" : null,
-                onChanged: (val) => email = val,
-                decoration: textInputDecoration.copyWith(hintText: "Email", icon: Icon(Icons.mail)),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.length < 6 ? "Enter a password greater than 6 characters" : null,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(hintText: "Password", icon: Icon(Icons.lock),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.teal))),
+                        validator: (val) => val.isEmpty ? "Enter an email" : null,
+                        onChanged: (val) => email = val,
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'PASSWORD',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.teal))),
+                        obscureText: true,
+                        validator: (val) => val.length < 6 ? "Enter a password greater than 6 characters" : null,
+                        onChanged: (val){
+                          setState(() {
+                            password = val;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 40.0),
+                      Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.tealAccent,
+                          color: Colors.teal,
+                          elevation: 7.0,
+                          child: RaisedButton(
+                            onPressed: () async {
+                              if(_formKey.currentState.validate()){
+                                setState(() {
+                                  loading = true;
+                                });
+                                dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                                if(result == null){
+                                  setState(() {
+                                    error = ("Could not login in with those credentials");
+                                    loading = false;
+                                  });
+                                }
+                              }
+                            },
+                            child: Center(
+                              child: Text(
+                                'LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40.0),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(40.0, 20.0, 0.0, 0.0),
+                        child: Text(error,
+                            style: TextStyle(
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red)),
+                      )
+                    ],
+                  )),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.teal,
-                child: Text(
-                  "Sign In",
-                  style: TextStyle(color: Colors.black),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'New to Fit4u?',
+                  style: TextStyle(fontFamily: 'Montserrat'),
                 ),
-                onPressed: () async {
-                  if(_formKey.currentState.validate()){
-                    setState(() {
-                      loading = true;
-                    });
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if(result == null) {
-                      setState(() {
-                        error = ("Could not log in with those credentials");
-                        loading = false;
-                       });
-                    }
-                  }
-                }
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red,
-                    fontSize: 14.0)
-              ),
-            ],
-          ),
-        )
-      ),
-    );
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    widget.toggleView();
+                    //Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
+    }
   }
 }
+
