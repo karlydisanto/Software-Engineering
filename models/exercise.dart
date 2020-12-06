e7ae3059d185f0da7acf8d47f7f21524d111afb3
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-12-06 16:41:29 -0500

    Update exercise.dart

--- models/exercise.dart
+++ models/exercise.dart
@@ -1,5 +1,16 @@
 class Exercise{
-  final String exercises;
 
-  Exercise({this.exercises});
+  final String name;
+  final String exercise1;
+  final String exercise2;
+  final String exercise3;
+  final String exercise4;
+  final String exercise5;
+  final String exercise6;
+  final String exercise7;
+  final String exercise8;
+  final String exercise9;
+  final String exercise10;
+
+  Exercise({this.name, this.exercise1, this.exercise2, this.exercise3, this.exercise4, this.exercise5, this.exercise6, this.exercise7, this.exercise8, this.exercise9, this.exercise10});
 }
