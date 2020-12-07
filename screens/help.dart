130b624e85c2a1dd35d49633310bff8bbc673112
dan-ojo <70729808+dan-ojo@users.noreply.github.com>
2020-12-06 20:48:11 -0500

    Add files via upload

--- screens/help.dart
+++ screens/help.dart
@@ -43,29 +43,45 @@ class HelpPage extends StatelessWidget {
                               ),
                               SizedBox(height: 10),
                               Text(
-                                "Here are some Frequently Asked Questions!",
+                                "Everthing you need to know so you can use Fit4U like a pro!",
                                 style: TextStyle(fontWeight: FontWeight.bold,
-                                fontSize: 15),
+                                fontSize: 18),
                               ),
 
                               SizedBox(height:25),
                               Text('I can’t login to Fit4U, what should I do?',style: TextStyle(fontWeight: FontWeight.bold,
-                                  fontSize: 15)),
+                                  fontSize: 16)),
                               SizedBox(height:5),
                               Text('Please check your username and password. If they are correct, you might close Fit4u and try it again for one or two times.',style: TextStyle(
-                                  fontSize: 12)),
+                                  fontSize: 14)),
                               SizedBox(height:25),
                               Text('I am unable to register, what should I do?',style: TextStyle(fontWeight: FontWeight.bold,
-                                  fontSize: 15)),
+                                  fontSize: 16)),
                               SizedBox(height:5),
                               Text('Please check that you are entering a valid email and password.',style: TextStyle(
-                                  fontSize: 12)),
+                                  fontSize: 14)),
                               SizedBox(height:25),
                               Text('Is filling out the survey mandatory?',style: TextStyle(fontWeight: FontWeight.bold,
-                                  fontSize: 15)),
+                                  fontSize: 16)),
                               SizedBox(height:5),
                               Text('In order to experience the full functionality of the application, it is recommended that you fill out the survey.',style: TextStyle(
-                                  fontSize: 12))
+                                  fontSize: 14)),
+                              SizedBox(height:25),
+                              Text('If Fit4U crashes several times, what should I do?',style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 16)),
+                              SizedBox(height:5),
+                              Text('Close it for a while. If this situation happens again, you can try to redownload it.',style: TextStyle(
+                                  fontSize: 14)),
+                              SizedBox(height:25),
+                              Text('If I exit the Fit4U will I loose my workouts and personal data?',style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 16)),
+                              SizedBox(height:5),
+                              Text('No, all user information is saved in a remote database',style: TextStyle(
+                                  fontSize: 14)),
+
+
+
+
 
                             ],
                           ),
